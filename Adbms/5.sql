87e5cbe826424ebd810277930fe70b1b7c66b1fa
dalbinmathew <dalbinmathew10@gmail.com>
2024-03-14 15:12:13 +0530

    assignment 5

--- Adbms/5.sql
+++ Adbms/5.sql
@@ -1,3 +1,16 @@
-create database store;
-use store;
-create table product (pdtid int primary key, pname varchar(50), price int, quantity int);
+-- create database store;
+-- use store;
+-- create table product (pdtid int primary key, pname varchar(50), price int, quantity int);
+-- call insertproduct(1,"choclate",100,2);
+-- call insertproduct(1,"rice",0,2);
+
+-- procedure
+
+-- CREATE DEFINER=`root`@`localhost` PROCEDURE `insertproduct`(in barcode int,
+-- in pname varchar(30),in price double,in quantity double)
+-- BEGIN
+-- if(price>0 && quantity >=0)
+-- then
+-- insert into product values(barcode,pname,price,quantity);
+-- end if;
+-- END
